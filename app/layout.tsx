9793c77961aa686981a1b868b7232969c65968a9
SalehAkaJim <saleh.7734@gmail.com>
2025-03-12 14:11:26 +0330

    Fix: home buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className={poppins.className}>
+      <head>
+        <meta property='og:title' content='Luna UI Kit' />
+
+        <meta property='og:type' content='website' />
+
+        <meta property='og:url' content='https://luna-ui-kit..com/' />
+
+        <meta
+          property='og:image:url'
+          content='https://luna-ui-kit.com/cover.webp'
+        />
+
+        <meta
+          property='og:description'
+          content="This open-source UI Kit is built on MUI (Material Design 2), and it's here to make your life easier! Whether you're designing or coding, this kit gives you a solid, flexible foundation to create modern web apps without starting from scratch. It's all about keeping things consistent, scalable, and super customizable. Oh, and we're always adding more cool components, so keep an eye out for updates. There's more awesome stuff on the way."
+        />
+      </head>
+
       <body>
         <AppRouterCacheProvider>
           <ThemeComponent>{children}</ThemeComponent>
