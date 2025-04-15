f561be7d1eb0515dce085b9727703fa31a54eaea
SalehAkaJim <saleh.7734@gmail.com>
2025-04-15 11:46:03 +0330

    Update: v2 - breadcrumb

--- app/docs/breadcrumb/page.tsx
+++ app/docs/breadcrumb/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from 'next'
-import { BreadcrumbSection } from './_components/section'
+import { Overview } from './_components/_overview'
+import { Customize } from './_components/_customize'
+import Code from './_components/_code'
 
 export const metadata: Metadata = {
   title: 'Luna | Breadcrumb',
@@ -9,5 +11,11 @@ export const metadata: Metadata = {
 }
 
 export default function Page() {
-  return <BreadcrumbSection />
+  return (
+    <>
+      <Overview />
+      <Code />
+      <Customize />
+    </>
+  )
 }
