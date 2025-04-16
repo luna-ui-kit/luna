8269b3ebcf37a1aced5bb896365c454a32c0fc21
SalehAkaJim <saleh.7734@gmail.com>
2025-04-16 11:30:21 +0330

    Clarity Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
 import { ThemeComponent } from '@/luna/theme/ThemeComponent'
 import { Quicksand } from 'next/font/google'
 import '@/luna/styles/globals.css'
-import Clarity from '@microsoft/clarity'
-
-Clarity.init(process.env.PROJECT_ID || '')
+import Script from 'next/script'
 
 const quicksand = Quicksand({
   weight: ['700', '600', '500', '400'],
@@ -34,6 +32,14 @@ export default function RootLayout({
           property='og:description'
           content="This open-source UI Kit is built on MUI (Material Design 2), and it's here to make your life easier! Whether you're designing or coding, this kit gives you a solid, flexible foundation to create modern web apps without starting from scratch. It's all about keeping things consistent, scalable, and super customizable. Oh, and we're always adding more cool components, so keep an eye out for updates. There's more awesome stuff on the way."
         />
+
+        <Script id='clarity'>
+          {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_PROJECT_ID}");`}
+        </Script>
       </head>
 
       <body>
