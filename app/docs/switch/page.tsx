9c27b9de74687bf525c80f76cbdf461f4fafdcb4
SalehAkaJim <saleh.7734@gmail.com>
2025-04-19 12:10:57 +0330

    Update: v2 - switch

--- app/docs/switch/page.tsx
+++ app/docs/switch/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from 'next'
-import { SwitchSection } from './_components/section'
+import { Overview } from './_components/_overview'
+import { Customize } from './_components/_customize'
+import Code from './_components/_code'
 
 export const metadata: Metadata = {
   title: 'Luna UI Kit | Switch',
@@ -9,5 +11,11 @@ export const metadata: Metadata = {
 }
 
 export default function Page() {
-  return <SwitchSection />
+  return (
+    <>
+      <Overview />
+      <Code />
+      <Customize />
+    </>
+  )
 }
