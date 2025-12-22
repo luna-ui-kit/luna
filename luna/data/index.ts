d04bc71094630a16c4e6322a03af7fa43754f593
SalehAkaJim <saleh.7734@gmail.com>
2025-12-22 10:00:22 +0330

    Build Error

--- luna/data/index.ts
+++ luna/data/index.ts
@@ -1,5 +1,5 @@
-export * from './_blogCards2'
-export * from './_blogCards2/types'
+export * from './_blogCards'
+export * from './_blogCards/types'
 
 export * from './_accordion'
 export * from './_accordion/types'
